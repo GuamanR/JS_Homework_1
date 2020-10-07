c5c0136fbf073fc5b8d21d462bd7d4546c397ef7
Richie <richie.guaman@gmail.com>
2020-10-07 08:15:05 -0400

    This is the Js code Hw

---
+++ Java_Hw_1.js
@@ -0,0 +1,45 @@
+// This is for problem one
+var grade=number(prompt("What grade did you make?"))
+if (grade>=90){
+console.log("You got an A")
+}
+else if (grade>=80){
+console.log("You got a B")
+}
+else if (grade>=70){
+console.log("You got a C")
+}
+else if (grade>=60){
+console.log("You got a D")
+}
+else (grade<60){
+console.log("You Failed, try again next time")
+}
+
+var grade=number(prompt("What grade did you make?"))
+switch (grade){
+
+case >=90:
+    
+console.log("You got an A")
+
+case >=80: 
+console.log("You got a B")
+
+case >=70:
+console.log("You got a C")
+
+case >=60:
+console.log("You got a D")
+
+case <60 :
+console.log("You Failed, try again next time")
+}
+
+var pizza= String(prompt("What is the best topping on pizz?"));
+
+if (pizza== "pineapple"){
+console.log("You know what's up")
+}
+else 
+console.log("reevaluate your life choices")
